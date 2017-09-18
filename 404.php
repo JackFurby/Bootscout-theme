e5cc50d63fca40d6e227b157a4b8fa8afdd088d6
Jack Furby <jackfurby@live.co.uk>
2017-09-18 12:06:41 +0100

    fixed footer covering sidebar on 404.php, added padding to cards on join-section-page-tempate.php

--- 404.php
+++ 404.php
@@ -1,22 +1,38 @@
 <?php get_header(); ?>
 
-<div class="container">
-  <div class="row">
-    
-    <div class="<?php if(is_active_sidebar('sidebar-widget-area')): ?>col-sm-8<?php else: ?>col-sm-12<?php endif; ?>">
-      <div id="content" role="main">
-        <div class="alert alert-warning">
-          <h1><i class="glyphicon glyphicon-warning-sign"></i> <?php _e('Error', 'b4st'); ?> 404</h1>
-          <p><?php _e('The page you were looking for does not exist.', 'b4st'); ?></p>
-        </div>
-      </div><!-- /#content -->
-    </div>
-    
-    <div class="col-sm-4" id="sidebar" role="navigation">
-       <?php get_sidebar(); ?>
-    </div>
-    
-  </div><!-- /.row -->
-</div><!-- /.container -->
+<!-- /#contaner and #wrap used for sticky footer -->
+<div id="container">
+    <div id="wrap">
+        <div class="container">
+          <div class="row">
+              <?php
+                  if ($options['sidebar'] != true) {
+                      if (is_active_sidebar('sidebar-widget-area')) {
+                          echo"<div class=col-sm-8>";
+                      } else {
+                          echo"<div class=col-sm-12>";
+                      }
+                  } else {
+                      echo"<div class=col-sm-12>";
+                  }
+              ?>
+              <div id="content" role="main">
+                <div class="alert alert-warning">
+                  <h1><i class="glyphicon glyphicon-warning-sign"></i> <?php _e('Error', 'b4st'); ?> 404</h1>
+                  <p><?php _e('The page you were looking for does not exist.', 'b4st'); ?></p>
+                </div>
+              </div><!-- /#content -->
+            </div>
+            <?php if ($options['sidebar'] != true) {
+              echo"
+                <div class=\"col-sm-4\" id=\"sidebar\" role=\"navigation\">";
+                    get_sidebar();
+                echo"</div>";
+            }
+            ?>
+          </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /#wrap -->
+</div><!-- /#container -->
 
 <?php get_footer(); ?>
