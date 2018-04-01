2875cbb342c4f69ab84bc3aa67f40910714d660e
Jack <FurbyJL@cardiff.ac.uk>
2018-04-01 12:22:47 +0100

    Updated banner and added it to other relevant pages. Removed banner page template

--- front-page.php
+++ front-page.php
@@ -79,12 +79,16 @@ if ($options['carousel'] == true) {
 	}
 } elseif ( has_post_thumbnail() ) {
 	$caption = esc_attr( get_post_meta( get_the_ID(), "featured_image_caption", true ) );
-	echo "<div class='d-flex text-center' style='height:400px;'>
-			<div class=\"align-self-center w-100 position-absolute\" style='z-index:10;'>
-				<h3 class=\"banner-caption h1 text-white\" style=\"font-size: 75px;\">$caption</h3>
-			</div>";
-	echo"<div id=\"banner-img\" class=\"img-responsive center-block overflow-hide position-absolute\" style=\"height:400px; filter:brightness(75%);\">";
-	the_post_thumbnail('full', array( 'class' => 'm-0 w-100' ));
+	echo "<div class='d-flex text-center banner'>";
+	if (!empty($caption) || trim($caption) != "") { // If caption exists and is not an empty string
+		echo "<div class=\"align-self-center w-100 position-absolute\" style='z-index:10;'>
+			<h3 class=\"banner-caption h1 text-white text-truncate pr-3 pl-3\">$caption</h3>
+		</div>
+		<div class=\"center-block position-absolute w-100 banner\" style=\"filter:brightness(75%);overflow:hidden;\">";
+	} else {
+		echo"<div class=\"center-block position-absolute w-100 banner\" style=\"overflow:hidden;\">";
+	}
+	the_post_thumbnail('full', array( 'class' => 'banner-img img-responsive' ));
 	echo"</div>";
 	echo"</div>";
 }
--- join-section-page-template.php
+++ join-section-page-template.php
@@ -1,8 +1,24 @@
 <?php /* Template Name: Section links page */ ?>
 <?php get_header(); ?>
 <?php
-    // get theme options
-    $options = get_option('scout_theme_options');
+	// get theme options
+	$options = get_option('scout_theme_options');
+
+	if ( has_post_thumbnail() ) {
+		$caption = esc_attr( get_post_meta( get_the_ID(), "featured_image_caption", true ) );
+		echo "<div class='d-flex text-center banner'>";
+		if (!empty($caption) || trim($caption) != "") { // If caption exists and is not an empty string
+			echo "<div class=\"align-self-center w-100 position-absolute\" style='z-index:10;'>
+				<h3 class=\"banner-caption h1 text-white text-truncate pr-3 pl-3\">$caption</h3>
+			</div>
+			<div class=\"center-block position-absolute w-100 banner\" style=\"filter:brightness(75%);overflow:hidden;\">";
+		} else {
+			echo"<div class=\"center-block position-absolute w-100 banner\" style=\"overflow:hidden;\">";
+		}
+		the_post_thumbnail('full', array( 'class' => 'banner-img img-responsive' ));
+		echo"</div>";
+		echo"</div>";
+	}
 ?>
 
 <!-- /#contaner and #wrap used for sticky footer -->
