9add4426007f8d8c5af11a09be5396e460050227
Jack <FurbyJL@cardiff.ac.uk>
2018-06-02 22:02:43 +0100

    redesigned cards for blog posts

--- loops/index-post.php
+++ loops/index-post.php
@@ -6,8 +6,9 @@ Used by index.php, category.php and author.php
 */
 ?>
 
-<article role="article" id="post_<?php the_ID()?>" <?php post_class("mb-5"); ?> >
-	<header>
+<article role="article" id="post_<?php the_ID()?>" <?php post_class("mb-3 card hover-shadow"); ?> >
+	<?php the_post_thumbnail("full", array("class" => "card-img-top")); ?>
+	<header class="card-body bg-light">
 		<h2 class="mw-100 break-word">
 			<a href="<?php the_permalink(); ?>">
 				<?php the_title()?>
@@ -19,11 +20,13 @@ Used by index.php, category.php and author.php
 			<i class="far fa-comment"></i>&nbsp;<a href="<?php comments_link(); ?>"><?php printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), '', 'b4st' ), number_format_i18n( get_comments_number() ) ); ?></a>
 		</p>
 	</header>
-	<div>
-		<?php the_post_thumbnail(); ?>
-
+	<hr class="mt-0">
+	<div class="card-body">
+		<a class="btn btn-circle bg-scout-blue text-white position-relative float-right hover-shadow" style="top: -63px;" href="<?php the_permalink(); ?>">
+			>
+		</a>
 		<?php if ( has_excerpt( $post->ID ) ) {
-			the_excerpt();
+			the_excerpt("m-0");
 		?><p><a href="<?php the_permalink(); ?>">
 		<?php _e( '&hellip; ' . __('Continue reading', 'b4st' ) . ' <i class="fas fa-arrow-right"></i>', 'b4st' ) ?>
 		</a></p>
