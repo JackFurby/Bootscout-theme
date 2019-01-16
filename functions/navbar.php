9fdd0bde85990482f208a8b393dbbb2309f6b447
Jack <FurbyJL@cardiff.ac.uk>
2019-01-16 23:56:37 +0000

    Updated nav bar to support sub menus, updated CSS  where I missed some colours

--- functions/navbar.php
+++ functions/navbar.php
@@ -8,24 +8,25 @@ class b4st_walker_nav_menu extends Walker_Nav_menu {
 	function start_lvl( &$output, $depth = 0, $args = array() ){ // ul
 		$indent = str_repeat("\t",$depth); // indents the outputted HTML
 		$submenu = ($depth > 0) ? ' sub-menu' : '';
-		$output .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
+		// if menu is a sub menu then add class dropdown-menu-sub
+		$output .= ( $depth > 0 ) ? "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth dropdown-menu-sub\">\n" : "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
 	}
 
-  function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ){ // li a span
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ){ // li a span
 
-    $indent = ( $depth ) ? str_repeat("\t",$depth) : '';
+		$indent = ( $depth ) ? str_repeat("\t",$depth) : '';
 
-    $li_attributes = '';
+		$li_attributes = '';
 		$class_names = $value = '';
 
-    $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 
 		$classes[] = ($args->walker->has_children) ? 'dropdown' : '';
 		$classes[] = ($item->current || $item->current_item_ancestor) ? 'active' : '';
 		$classes[] = 'nav-item';
 		$classes[] = 'nav-item-' . $item->ID;
 		if( $depth && $args->walker->has_children ){
-			$classes[] = 'dropdown-menu dropdown-menu-right';
+			$classes[] = 'dropdown-menu-right dropdown-submenu';
 		}
 
 		$class_names =  join(' ', apply_filters('nav_menu_css_class', array_filter( $classes ), $item, $args ) );
@@ -43,8 +44,19 @@ class b4st_walker_nav_menu extends Walker_Nav_menu {
 
 		$attributes .= ( $args->walker->has_children ) ? ' class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="nav-link"';
 
+
 		$item_output = $args->before;
-		$item_output .= ( $depth > 0 ) ? '<a class="dropdown-item"' . $attributes . '>' : '<a' . $attributes . '>';
+		// style link depending if link is just a link, opens a menu or opens a sub menu
+		if ($depth > 0) {
+			if ($args->walker->has_children != true) {
+				$item_output .= '<a class="dropdown-item"' . $attributes . '>';
+			} else {
+				$item_output .= '<a class="dropdown-item dropdown-toggle"' . $attributes . '>';
+			}
+
+		} else {
+			$item_output .= '<a' . $attributes . '>';
+		}
 		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
 		$item_output .= '</a>';
 		$item_output .= $args->after;
--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -12,42 +12,42 @@
 <?php b4st_navbar_before();?>
 
 <nav id="navbar" class="navbar navbar-expand-md navbar-dark bg-scout-purple solid-shadow">
-  <div class="container">
+	<div class="container">
 
-	<?php if ( function_exists( 'the_custom_logo' ) ) {
-		$custom_logo_id = get_theme_mod( 'custom_logo' );
-		$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-		$link = esc_url( home_url('/') );
-		if ( has_custom_logo() ) {
-			echo"<div><a href=\"$link\"><img class=\"navbar-brand\" src=\"$image[0]\" style=\"height:65px\"></a></div>";
-		} else {
-			echo"<a class=\"navbar-brand\" href=\""; echo esc_url( home_url('/') ); echo"\">";
-				bloginfo('name');
-				echo"</a>";
-		}
-	}?>
+		<?php if ( function_exists( 'the_custom_logo' ) ) {
+			$custom_logo_id = get_theme_mod( 'custom_logo' );
+			$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+			$link = esc_url( home_url('/') );
+			if ( has_custom_logo() ) {
+				echo"<div><a href=\"$link\"><img class=\"navbar-brand\" src=\"$image[0]\" style=\"height:65px\"></a></div>";
+			} else {
+				echo"<a class=\"navbar-brand\" href=\""; echo esc_url( home_url('/') ); echo"\">";
+					bloginfo('name');
+					echo"</a>";
+			}
+		}?>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-    <div class="collapse navbar-collapse" id="navbarDropdown">
-      <?php
-        wp_nav_menu( array(
-          'theme_location'  => 'navbar',
-          'container'       => false,
-          'menu_class'      => '',
-          'fallback_cb'     => '__return_false',
-          'items_wrap'      => '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
-          'depth'           => 2,
-          'walker'          => new b4st_walker_nav_menu()
-        ) );
-      ?>
+		<div class="collapse navbar-collapse" id="navbarDropdown">
+			<?php
+				wp_nav_menu( array(
+					'theme_location'  => 'navbar',
+					'container'       => false,
+					'menu_class'      => '',
+					'fallback_cb'     => '__return_false',
+					'items_wrap'      => '<ul id="%1$s" class="navbar-nav mr-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
+					'depth'           => 0,
+					'walker'          => new b4st_walker_nav_menu()
+				) );
+			?>
 
-      <?php b4st_navbar_search();?>
-    </div>
+			<?php b4st_navbar_search();?>
+		</div>
 
-  </div>
+	</div>
 </nav>
 
 <?php b4st_navbar_after();?>
