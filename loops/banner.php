fa9f2f7c86df5458409768269c8479e55980d905
Jack <FurbyJL@cardiff.ac.uk>
2019-04-14 20:40:35 +0100

    Updated banner to work better on smaller displays

--- loops/banner.php
+++ loops/banner.php
@@ -6,7 +6,7 @@
 		$caption = esc_attr( get_post_meta( get_the_ID(), "featured_image_caption", true ) );
 		echo "<div class='text-center banner'>";
 		echo"<div class=\"center-block w-100 banner\" style=\"overflow:hidden;\">";
-		the_post_thumbnail('full', array( 'class' => 'banner-img img-responsive' ));
+		the_post_thumbnail('full', array( 'class' => 'banner-img img-fluid' ));
 		echo"</div>";
 		echo"</div>";
 	}
