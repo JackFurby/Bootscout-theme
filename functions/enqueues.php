62dbafd51db45bba019b1ef1b138056e58a186aa
Jack <FurbyJL@cardiff.ac.uk>
2019-05-04 10:45:23 +0100

    Updated to B4st v2.7

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -8,17 +8,13 @@ if ( ! function_exists('b4st_enqueues') ) {
 
 		// Styles
 
-		wp_register_style('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css', false, '4.1.3', null);
+		wp_register_style('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css', false, '4.3.1', null);
 		wp_enqueue_style('bootstrap');
-
-		wp_register_style('fontawesome5', 'https://use.fontawesome.com/releases/v5.6.1/css/all.css', false, '5.6.1', null);
+		wp_register_style('fontawesome5', 'https://use.fontawesome.com/releases/v5.8.1/css/all.css', false, '5.8.1', null);
 		wp_enqueue_style('fontawesome5');
-
 		wp_register_style('bootstrap_override', get_template_directory_uri() . '/theme/css/bootstrap_override.css', false, null);
 		wp_enqueue_style('bootstrap_override');
-
 		wp_enqueue_style( 'gutenberg-blocks', get_template_directory_uri() . '/theme/css/blocks.css' );
-
 		wp_register_style('b4st', get_template_directory_uri() . '/theme/css/b4st.css', false, null);
 		wp_enqueue_style('b4st');
 
@@ -26,13 +22,10 @@ if ( ! function_exists('b4st_enqueues') ) {
 
 		wp_register_script('modernizr',  'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', false, '2.8.3', true);
 		wp_enqueue_script('modernizr');
-
 		wp_enqueue_script('jquery');
-
-		wp_register_script('bootstrap-bundle', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js', false, '4.1.3', true);
+		wp_register_script('bootstrap-bundle', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js', false, '4.3.1', true);
 		wp_enqueue_script('bootstrap-bundle');
 		// (The Bootstrap JS bundle contains Popper JS.)
-
 		wp_register_script('b4st', get_template_directory_uri() . '/theme/js/b4st.js', false, null, true);
 		wp_enqueue_script('b4st');
 
