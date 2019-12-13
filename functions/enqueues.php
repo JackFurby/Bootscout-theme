f968442db1bfcd1db359a3ee9d529121dba584e6
Jack <furbyjl@cardiff.ac.uk>
2019-12-13 00:25:36 +0000

    Updated Bootstrap, Fontawesome and Modernizr

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -8,9 +8,9 @@ if ( ! function_exists('b4st_enqueues') ) {
 
 		// Styles
 
-		wp_register_style('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css', false, '4.3.1', null);
+		wp_register_style('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css', false, '4.4.1', null);
 		wp_enqueue_style('bootstrap');
-		wp_register_style('fontawesome5', 'https://use.fontawesome.com/releases/v5.10.1/css/all.css', false, '5.10.1', null);
+		wp_register_style('fontawesome5', 'https://use.fontawesome.com/releases/v5.12.0/css/all.css', false, '5.12.0', null);
 		wp_enqueue_style('fontawesome5');
 		wp_register_style('bootstrap_override', get_template_directory_uri() . '/theme/css/bootstrap_override.css', false, null);
 		wp_enqueue_style('bootstrap_override');
@@ -20,10 +20,10 @@ if ( ! function_exists('b4st_enqueues') ) {
 
 		// Scripts
 
-		wp_register_script('modernizr',  'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', false, '2.8.3', true);
+		wp_register_script('modernizr',  'https://cdnjs.cloudflare.com/ajax/libs/modernizr/3.5.0/modernizr.min.js', false, '3.5.0', true);
 		wp_enqueue_script('modernizr');
 		wp_enqueue_script('jquery');
-		wp_register_script('bootstrap-bundle', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js', false, '4.3.1', true);
+		wp_register_script('bootstrap-bundle', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js', false, '4.4.1', true);
 		wp_enqueue_script('bootstrap-bundle');
 		// (The Bootstrap JS bundle contains Popper JS.)
 		wp_register_script('b4st', get_template_directory_uri() . '/theme/js/b4st.js', false, null, true);
