fd9c867e2372cdb629455f180a3d6b725645df74
Jack <furbyjl@cardiff.ac.uk>
2020-02-01 13:53:47 +0000

    Added navbar colour change to search widget
    * search widget now changes colour to match search in navbar

--- functions/search-widget.php
+++ functions/search-widget.php
@@ -6,11 +6,14 @@
 if ( ! function_exists('b4st_search_form') ) {
 
 	function b4st_search_form( $form ) {
+		$options = get_option('scout_theme_options');
+		$bg_colour = get_navbar_search_bg_colour($options['navColour']);
+		$btn_colour = get_navbar_search_btn_colour($options['navColour']);
 		$form = '<form class="form-inline" role="search" method="get" id="searchform" action="' . home_url('/') . '" >
 			<div class="input-group w-100">
-				<input class="form-control" type="text" value="' . get_search_query() . '" placeholder="' . esc_attr_x('Search', 'b4st') . '..." name="s" id="s">
+				<input class="bg-scout-' . $bg_colour . ' form-control" type="text" value="' . get_search_query() . '" placeholder="' . esc_attr_x('Search', 'b4st') . '..." name="s" id="s">
 				<div class="input-group-append">
-					<button type="submit" id="searchsubmit" value="'. esc_attr_x('Search', 'b4st') .'" class="btn btn-scout-teal text-white">
+					<button type="submit" id="searchsubmit" value="'. esc_attr_x('Search', 'b4st') .'" class="btn btn-scout-' . $btn_colour . ' border">
 						<i class="fas fa-search"></i>
 					</button>
 				</div>
