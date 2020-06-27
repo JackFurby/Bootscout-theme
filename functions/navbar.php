c5f7d2b21cf76253ac4bed2b42be65b2ff672f48
Jack <jackfurby@live.co.uk>
2020-06-27 18:51:55 +0100

    updated mavbar menus to show on hover at depth 0

--- functions/navbar.php
+++ functions/navbar.php
@@ -14,20 +14,19 @@ class b4st_walker_nav_menu extends Walker_Nav_menu {
 
 	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ){ // li a span
 
-		$indent = ( $depth ) ? str_repeat("\t",$depth) : '';
 		$options = get_option('scout_theme_options');
+		$indent = ( $depth ) ? str_repeat("\t",$depth) : '';  # Indent code
 
 		$li_attributes = '';
 		$class_names = $value = '';
-
 		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 
-		$classes[] = ($args->walker->has_children) ? 'dropdown' : '';
-		$classes[] = ($item->current || $item->current_item_ancestor) ? 'active' : '';
+		$classes[] = ($args->walker->has_children) ? 'dropdowns dropdown-submenu' : '';  # Link has children
+		$classes[] = ($item->current || $item->current_item_ancestor) ? 'active' : '';  # On this items page
 		$classes[] = 'nav-item';
 		$classes[] = 'nav-item-' . $item->ID;
-		if( $depth && $args->walker->has_children ){
-			$classes[] = 'dropdown-menu-right dropdown-submenu';
+		if( $depth && $args->walker->has_children ){ # Link has children and is a child of another menu
+			$classes[] = 'dropdown-menu-right';
 		}
 
 		$class_names =  join(' ', apply_filters('nav_menu_css_class', array_filter( $classes ), $item, $args ) );
@@ -45,7 +44,7 @@ class b4st_walker_nav_menu extends Walker_Nav_menu {
 
 		$link_text_colour = get_navbar_text_colour($options['navColour']);
 
-		$attributes .= ( $args->walker->has_children ) ? ' class="link-text-'.$link_text_colour.' nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="link-text-'.$link_text_colour.' nav-link"';
+		$attributes .= ( $args->walker->has_children ) ? ' class="link-text-'.$link_text_colour.' nav-link dropdown-toggle" ' : ' class="link-text-'.$link_text_colour.' nav-link"';
 
 
 		$item_output = $args->before;
@@ -61,6 +60,7 @@ class b4st_walker_nav_menu extends Walker_Nav_menu {
 				$item_output .= '<a class="dropdown-item dropdown-item-scout-'. $focus_colour .' dropdown-toggle"' . (! empty( $item->url ) ? ' href="' . esc_attr($item->url) . '"' : '') . '>';
 			}
 
+		# If link is at depth 0 (can still be a dropdown)
 		} else {
 			$item_output .= '<a' . $attributes . '>';
 		}
