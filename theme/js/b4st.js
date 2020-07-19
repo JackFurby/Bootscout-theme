ee20fe5ee95da3567a3afdd5fab9b08e0c95d85b
Jack <jackfurby@live.co.uk>
2020-07-19 17:15:01 +0100

    added JS to allow multilayer dropdowns to remain open on click

--- theme/js/b4st.js
+++ theme/js/b4st.js
@@ -24,16 +24,20 @@
 
 		// Navbar
 
-		//$('.btn-group').on('hide.bs.dropdown', function (e) {
-		//	console.log("hi");
-		//	if (e.clickEvent) {
-		//		e.preventDefault();
-		//}
+		// allows nested dropdowns to be open at the same time
+		$('.dropdown-menu button.dropdown-toggle').on('click', function(e) {
+			if (!$(this).next().hasClass('show')) {
+				$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+			}
+			var $subMenu = $(this).next(".dropdown-menu");
+			$subMenu.toggleClass('show');
 
-		$(document).on('click', '.btn-group', function (e) {
-			e.stopPropagation();
+			$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+				$('.dropdown-submenu .show').removeClass("show");
+			});
+
+			return false;
 		});
-});
 
 	});
 
