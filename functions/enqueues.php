dbc9b6572e7ae3c87c9a74daafb52090523a5a88
Jack <jackfurby@live.co.uk>
2022-02-02 19:58:57 +0000

    renamed site css files

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -8,7 +8,7 @@ function bootscout_theme_scripts() {
 	wp_register_style('BootstrapIcons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css', false, '1.5.0', null);
 	wp_enqueue_style('BootstrapIcons');
 	wp_enqueue_style('gutenberg-blocks', get_template_directory_uri() . '/theme/css/blocks.css');
-	wp_register_style('theme', get_template_directory_uri() . '/theme/css/b4st.css', false, null);
+	wp_register_style('theme', get_template_directory_uri() . '/theme/css/site.css', false, null);
 	wp_enqueue_style('theme');
 	wp_enqueue_script("jquery");  // use WordPress built in jQuery
 
@@ -17,7 +17,7 @@ function bootscout_theme_scripts() {
 	wp_register_script('bootstrap5', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', false, '5.0.2', true);
 	wp_enqueue_script('bootstrap5');
 	// (The Bootstrap JS bundle contains Popper JS.)
-	wp_register_script('theme', get_template_directory_uri() . '/theme/js/b4st.js', false, null, true);
+	wp_register_script('theme', get_template_directory_uri() . '/theme/js/site.js', false, null, true);
 	wp_enqueue_script('theme');
 }
 
