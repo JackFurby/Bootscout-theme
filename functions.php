c445c3516e277d892ff4eb8231b0479e004cb0ea
Jack <jackfurby@live.co.uk>
2022-02-11 23:55:09 +0000

    added remaining templates and removed legacy SCSS

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 require get_template_directory() . '/functions/setup.php';
 require get_template_directory() . '/functions/enqueues.php';
+require get_template_directory() . '/functions/utils.php';
 
 // Header block patterns.
 require_once 'inc/header-block-patterns.php';
--- functions/enqueues.php
+++ functions/enqueues.php
@@ -5,7 +5,7 @@
 function bootscout_theme_scripts() {
 	// Styles
 
-	wp_register_style('BootstrapIcons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css', false, '1.5.0', null);
+	wp_register_style('BootstrapIcons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css', false, '1.7.0', null);
 	wp_enqueue_style('BootstrapIcons');
 	wp_enqueue_style('gutenberg-blocks', get_template_directory_uri() . '/theme/css/blocks.css');
 	wp_register_style('theme', get_template_directory_uri() . '/theme/css/site.css', false, null);
