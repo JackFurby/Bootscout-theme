5dfe87a25da4269525b92cab91fc3e33e7cb9c16
Jack <jackfurby@live.co.uk>
2022-02-13 20:46:30 +0000

    updated posts layouts, updated footer, updated search, removed old javascript

--- theme/js/site.js
+++ theme/js/site.js
@@ -9,16 +9,6 @@
 
 	$(document).ready(function() {
 
-		// Comments
-
-		$('.commentlist li').addClass('card mb-3');
-		$('.comment-reply-link').addClass('btn btn-secondary');
-
-		// Forms
-
-		$('select, input[type=text], input[type=email], input[type=password], textarea').addClass('form-control');
-		$('input[type=submit]').addClass('btn btn-primary');
-
 		// Pagination fix for ellipsis
 
 		$('.pagination .dots').addClass('page-link').parent().addClass('disabled');
