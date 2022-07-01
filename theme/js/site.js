f5274137c8dc6970c4c9a961979f118cf1572b82
Jack <jackfurby@live.co.uk>
2022-07-01 16:43:35 +0100

    fixed outline button hover activating when not over button

--- theme/js/site.js
+++ theme/js/site.js
@@ -9,21 +9,22 @@ var outlineButtons = document.getElementsByClassName("wp-block-button is-style-o
 
 // set text colour to white and background to text colour on hover
 var outlineButtonHover = function() {
-	var btnColor = getComputedStyle(this.getElementsByTagName("a")[0]).color;
-	this.getElementsByTagName("a")[0].style.setProperty("background-color", btnColor, "important")
-	this.getElementsByTagName("a")[0].style.setProperty("color", "white", "important");
+	var btnColor = getComputedStyle(this).color;
+	this.style.setProperty("background-color", btnColor, "important")
+	this.style.setProperty("color", "white", "important");
 };
 
 // revert hover colour to how they were before
 var outlineButtonHoverOver = function() {
-	var btnColor = getComputedStyle(this.getElementsByTagName("a")[0]).backgroundColor;
-	this.getElementsByTagName("a")[0].style.setProperty("background-color", "transparent", "important");
-	this.getElementsByTagName("a")[0].style.setProperty("color", btnColor, "important");
+	var btnColor = getComputedStyle(this).backgroundColor;
+	this.style.setProperty("background-color", "transparent", "important");
+	this.style.setProperty("color", btnColor, "important");
 };
 
 for (var i = 0; i < outlineButtons.length; i++) {
-	outlineButtons[i].addEventListener('mouseover', outlineButtonHover, false);
-	outlineButtons[i].addEventListener('mouseout', outlineButtonHoverOver, false);
+	console.log(outlineButtons[i]);
+	outlineButtons[i].getElementsByTagName("a")[0].addEventListener('mouseover', outlineButtonHover, false);
+	outlineButtons[i].getElementsByTagName("a")[0].addEventListener('mouseout', outlineButtonHoverOver, false);
 
 	// if outline button has background, set background to transparent
 	outlineButtons[i].getElementsByTagName("a")[0].style.setProperty("background-color", "transparent", "important");
