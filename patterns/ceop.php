257e1c5b6321217220479967df7a438ccfa252d8
Jack <jackfurby@live.co.uk>
2023-11-11 11:45:49 +0000

    added section banner pattern, updated version number

--- patterns/ceop.php
+++ patterns/ceop.php
@@ -7,25 +7,29 @@
  */
 ?>
 <!-- wp:columns {"verticalAlignment":null,"backgroundColor":"scout-yellow"} -->
-<div class="wp-block-columns has-scout-yellow-background-color has-background"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:paragraph {"fontSize":"x-large"} -->
-<p class="has-x-large-font-size">Young people first: safeguarding and safety in Scouting</p>
-<!-- /wp:paragraph -->
+<div class="wp-block-columns has-scout-yellow-background-color has-background">
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:paragraph {"fontSize":"x-large"} -->
+		<p class="has-x-large-font-size">Young people first: safeguarding and safety in Scouting</p>
+		<!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"medium"} -->
-<p class="has-medium-font-size" style="font-style:normal;font-weight:300">Wherever we go and whatever we do, we put young people’s safety and wellbeing first.</p>
-<!-- /wp:paragraph -->
+		<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"medium"} -->
+		<p class="has-medium-font-size" style="font-style:normal;font-weight:300">Wherever we go and whatever we do, we put young people’s safety and wellbeing first.</p>
+		<!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"medium"} -->
-<p class="has-medium-font-size" style="font-style:normal;font-weight:300"><a href="https://www.scouts.org.uk/information-for-parents/stay-safe" target="_blank" rel="noreferrer noopener"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-scout-blue-color">Here's how</mark></a></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+		<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"medium"} -->
+		<p class="has-medium-font-size" style="font-style:normal;font-weight:300"><a href="https://www.scouts.org.uk/information-for-parents/stay-safe" target="_blank" rel="noreferrer noopener"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-scout-blue-color">Here's how</mark></a></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center", "width":"300px"} -->
-<div class="wp-block-column is-vertically-aligned-center"  style="flex-basis:300px">
-  <!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"custom"} -->
-  <figure class="wp-block-image aligncenter size-full"><a href="https://www.ceop.police.uk/safety-centre/"><img src="<?php echo esc_url( get_template_directory_uri()); ?>/theme/images/CEOP.png" alt="" /></a></figure>
-  <!-- /wp:image -->
+	<!-- wp:column {"verticalAlignment":"center", "width":"300px"} -->
+	<div class="wp-block-column is-vertically-aligned-center"  style="flex-basis:300px">
+		<!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"custom"} -->
+		<figure class="wp-block-image aligncenter size-full"><a href="https://www.ceop.police.uk/safety-centre/"><img src="<?php echo esc_url( get_template_directory_uri()); ?>/theme/images/CEOP.png" alt="CEOP logo"/></a></figure>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:column -->
 </div>
-<!-- /wp:column --></div>
 <!-- /wp:columns -->
---
+++ patterns/section_banner.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Title: Section banner
+ * Slug: bootscout-theme/section_banner
+ * Categories: bootscout, banner
+ */
+?>
+
+<!-- wp:columns {"align":"full","backgroundColor":"secondary"} -->
+<div class="wp-block-columns alignfull has-secondary-background-color has-background"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:image -->
+<figure class="wp-block-image"><img src="<?php echo esc_url( get_template_directory_uri()); ?>/theme/images/squirrels-primary-logo-white-png.png" alt="squirrel scouts logo"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color"><strong>4-6 years</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:image -->
+<figure class="wp-block-image"><img src="<?php echo esc_url( get_template_directory_uri()); ?>/theme/images/beavers-logo-white-png.png" alt="beaver scouts logo"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color"><strong>6-8 years</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:image -->
+<figure class="wp-block-image"><img src="<?php echo esc_url( get_template_directory_uri()); ?>/theme/images/cubs-logo-white-png.png" alt="cub scouts logo"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color"><strong>8-10 1/2 years</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:image -->
+<figure class="wp-block-image"><img src="<?php echo esc_url( get_template_directory_uri()); ?>/theme/images/scouts-logo-white-png.png" alt="scouts logo"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color"><strong>10 1/2-14 years</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:image -->
+<figure class="wp-block-image"><img src="<?php echo esc_url( get_template_directory_uri()); ?>/theme/images/explorers-logo-white-png.png" alt="explorer scouts logo"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color"><strong>14-18 years</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:image -->
+<figure class="wp-block-image"><img src="<?php echo esc_url( get_template_directory_uri()); ?>/theme/images/network-logo-white-png.png" alt="network scouts logo"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color"><strong>18-25 years</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
