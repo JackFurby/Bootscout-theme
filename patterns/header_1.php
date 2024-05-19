6354d60c34c792c30e59832321011f8524629994
Jack <jackfurby@live.co.uk>
2024-05-19 09:49:46 +0100

    updated header template to use patterns, added new header pattern

---
+++ patterns/header_1.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Title: Bootscout header 1
+ * Slug: bootscout-theme/header_1
+ * Categories: bootscout, header
+ */
+?>
+
+
+<!-- wp:columns {"verticalAlignment":"center", "align":"full","style":{"spacing":{"padding":{"top":"0px","right":"2.38em","bottom":"0px","left":"2.38em"},"margin":{"bottom":"0px"}}},"backgroundColor":"primary", "className":"gap-0"} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-center has-primary-background-color has-background gap-0"
+	style="padding-top:0px;padding-right:2.38em;padding-bottom:0px;padding-left:2.38em;margin-bottom:0px;">
+	<!-- wp:column {"verticalAlignment":"center","className":"site-title-column", "style":{"spacing":{"margin":{"right":"2em"}}}} -->
+	<div class="wp-block-column is-vertically-aligned-center site-title-column" style="margin-right:2em;">
+		<!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"margin":{"bottom":"0px"}}},"fontSize":"large","textColor":"white"} /-->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"verticalAlignment":"center", "style":{"spacing":{"margin":{"right":"10px"}}}} -->
+	<div class="wp-block-column is-vertically-aligned-center" style="margin-right:10px;">
+		<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+		<div class="wp-block-group">
+			<!-- wp:navigation {"textColor":"white", "icon":"menu", "style":{"typography":{"fontStyle":"medium","fontWeight":"600"}}, "layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left"}} -->
+			<!-- /wp:navigation -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"verticalAlignment":"center","width":"270px","style":{"spacing":{"padding":{"top":"1.25em","bottom":"1.25em"}}}} -->
+	<div class="wp-block-column is-vertically-aligned-center has-background"
+		style="padding-top:1.25em;padding-bottom:1.25em;flex-basis:270px">
+		<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true, "buttonPosition":"button-inside", "align":"center"} /-->
+	</div>
+	<!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
