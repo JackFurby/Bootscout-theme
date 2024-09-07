5bb1012834a9135799c2a49ba3e6a5b1743858cf
Jack <jackfurby@live.co.uk>
2024-09-07 16:52:45 +0100

    added scout-ish header and fixed issue where search button was half hidden

---
+++ patterns/scoutish_header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Scout-ish header
+ * Slug: bootscout-theme/scoutish_header
+ * Categories: bootscout, header
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull">
+	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"0","left":"0","top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"2.38em","left":"2.38em","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"margin":{"top":"0px","bottom":"0px"}},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+		<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:var(--wp--preset--spacing--40);padding-right:2.38em;padding-bottom:var(--wp--preset--spacing--40);padding-left:2.38em">
+
+			<!-- wp:site-title {"textAlign":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|grey-80"}}},"spacing":{"margin":{"bottom":"0px"}}},"textColor":"grey-80","fontSize":"large"} /-->
+
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+			<div class="wp-block-group">
+				<!-- wp:navigation {textColor":"grey-80","icon":"menu","fontSize":"medium","layout":{"type":"flex","justifyContent":"right"}} /-->
+
+				<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-only","buttonUseIcon":true,"isSearchFieldHidden":true,"align":"center","style":{"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|grey-80"}}},"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"},"left":{"color":"#CCCCCC","width":"2px"},"top":{},"right":{},"bottom":{}}},"backgroundColor":"white","textColor":"grey-80"} /-->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
