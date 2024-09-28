33c02a24e2f894065729d1cc3879ac2d2cd55b66
Jack <jackfurby@live.co.uk>
2024-09-28 13:11:29 +0100

    made outline buttons easier to colour

--- theme/js/site.js
+++ theme/js/site.js
@@ -10,22 +10,32 @@ var outlineButtons = document.getElementsByClassName("wp-block-button is-style-o
 // set text colour to white and background to text colour on hover
 var outlineButtonHover = function() {
 	var btnColor = getComputedStyle(this).color;
+	var btnBgColor = getComputedStyle(this).backgroundColor;
 	this.style.setProperty("background-color", btnColor, "important")
-	this.style.setProperty("color", "white", "important");
+
+	// if outline button has no background, set background to white
+	if (this.classList.contains("has-background") == false) {
+		this.style.setProperty("color", "white", "important");
+	} else {
+		this.style.setProperty("color", btnBgColor, "important");
+	}
 };
 
 // revert hover colour to how they were before
 var outlineButtonHoverOver = function() {
 	var btnColor = getComputedStyle(this).backgroundColor;
-	this.style.setProperty("background-color", "transparent", "important");
+	var btnBgColor = getComputedStyle(this).color;
+
+	// if outline button has no background, set background to transparent
+	if (this.classList.contains("has-background") == false) {
+		this.style.setProperty("background-color", "transparent", "important");
+	} else {
+		this.style.setProperty("background-color", btnBgColor, "important");
+	}
 	this.style.setProperty("color", btnColor, "important");
 };
 
 for (var i = 0; i < outlineButtons.length; i++) {
-	console.log(outlineButtons[i]);
 	outlineButtons[i].getElementsByTagName("a")[0].addEventListener('mouseover', outlineButtonHover, false);
 	outlineButtons[i].getElementsByTagName("a")[0].addEventListener('mouseout', outlineButtonHoverOver, false);
-
-	// if outline button has background, set background to transparent
-	outlineButtons[i].getElementsByTagName("a")[0].style.setProperty("background-color", "transparent", "important");
 }
