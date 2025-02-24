4c383eb02da032cf5c422ad40be0a9d58451b999
Jack <jackfurby@live.co.uk>
2025-02-24 13:13:44 +0000

    added initial fixes to make theme compatible to woocommerce with blocks

--- functions/setup.php
+++ functions/setup.php
@@ -17,12 +17,12 @@ if ( ! function_exists( 'bootscout_theme_support' ) ) :
 		add_theme_support( 'custom-units' );
 
 		// plugins
+		add_theme_support( 'woocommerce' );
 		remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 		remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
 		add_action('woocommerce_before_main_content', 'bootscout_woocommerce_output_content_wrapper', 10);
 		add_action('woocommerce_after_main_content', 'bootscout_woocommerce_output_content_wrapper_end', 10);
-
 	}
 	add_action( 'after_setup_theme', 'bootscout_theme_support' );
 endif;
