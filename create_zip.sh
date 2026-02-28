31405ab0b0c09adfbd5c701b8cd16c23c455b28e
Jack Furby <jackfurby@live.co.uk>
2026-02-28 13:01:07 +0000

    removed gulpfile, updated create_zip

--- create_zip.sh
+++ create_zip.sh
@@ -4,9 +4,6 @@ vn=$(grep "^Version" style.css | grep -oe '\([0-9.]*\)')
 # delete existing release zip files
 rm Bootscout-theme*.zip
 
-# compile css
-gulp css
-
 # make temp dir to mimic folder structure in zip archaive
 mkdir ./Bootscout-theme
 rsync -r --exclude='Bootscout-theme' --exclude='node_modules' ./* Bootscout-theme
@@ -19,10 +16,8 @@ zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/package-lock.json"
 zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/package.json"
 zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/Bootscout-theme*.zip"
 zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/.gitignore"
-zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/gulpfile.js"
 zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/create_zip.sh"
-zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/bootstrap/scss/*"
-zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/theme/scss/*"
+zip --delete Bootscout-theme-${vn}.zip "Bootscout-theme/CLAUDE.md"
 
 # delete temp dir
 rm -rf Bootscout-theme
