b3b386207156acaaa9ecab0ee4c140405338d484
Jack Furby <jackfurby@live.co.uk>
2026-02-28 17:47:58 +0000

    replaced old footer design with new one in template parts

--- patterns/footer_1.php
+++ patterns/footer_1.php
@@ -56,71 +56,72 @@
 			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0">Look wider</h3>
 			<!-- /wp:heading -->
 
-			<!-- wp:navigation {textColor":"white","overlayMenu":"never","overlayTextColor":"grey-80","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left","orientation":"vertical"}} /--></div>
-			<!-- /wp:column -->
-
-			<!-- wp:column {"verticalAlignment":"top"} -->
-			<div class="wp-block-column is-vertically-aligned-top">
-				<!-- wp:heading {"level":3} -->
-				<h3 class="wp-block-heading">Contact us</h3>
-				<!-- /wp:heading -->
-
-				<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"24px","bottom":"var:preset|spacing|20"}}}} -->
-				<p style="margin-top:24px;margin-bottom:var(--wp--preset--spacing--20)">Email:</p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
-				<p style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)">Phone:</p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
-				<p style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)">Address:</p>
-				<!-- /wp:paragraph -->
-			</div>
-			<!-- /wp:column -->
+			<!-- wp:navigation {textColor":"white","overlayMenu":"never","overlayTextColor":"grey-80","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left","orientation":"vertical"}} /-->
 		</div>
-		<!-- /wp:columns -->
+		<!-- /wp:column -->
 
-		<!-- wp:separator {"className":"is-style-wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"0"}},"color":{"background":"#ffffff5c"}}} -->
-		<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:0;background-color:#ffffff5c;color:#ffffff5c"/>
-		<!-- /wp:separator -->
+		<!-- wp:column {"verticalAlignment":"top"} -->
+		<div class="wp-block-column is-vertically-aligned-top">
+			<!-- wp:heading {"level":3} -->
+			<h3 class="wp-block-heading">Contact us</h3>
+			<!-- /wp:heading -->
 
-		<!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
-		<div class="wp-block-columns alignwide">
-			<!-- wp:column -->
-			<div class="wp-block-column">
-				<!-- wp:group {"layout":{"type":"flex","allowOrientation":false,"justifyContent":"center"}} -->
-				<div class="wp-block-group">
-					<!-- wp:paragraph -->
-					<p>©</p>
-					<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"24px","bottom":"var:preset|spacing|20"}}}} -->
+			<p style="margin-top:24px;margin-bottom:var(--wp--preset--spacing--20)">Email:</p>
+			<!-- /wp:paragraph -->
 
-					<!-- wp:shortcode -->
-					[current-year]
-					<!-- /wp:shortcode -->
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
+			<p style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)">Phone:</p>
+			<!-- /wp:paragraph -->
 
-					<!-- wp:site-title {"level":0} /-->
-				</div>
-				<!-- /wp:group -->
-
-				<!-- wp:paragraph {"className":"has-text-align-center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-				<p class="has-text-align-center" style="margin-top:0;margin-bottom:0">Charity number: </p>
-				<!-- /wp:paragraph -->
-			</div>
-			<!-- /wp:column -->
-
-			<!-- wp:column -->
-			<div class="wp-block-column"></div>
-			<!-- /wp:column -->
-
-			<!-- wp:column {"verticalAlignment":"bottom"} -->
-			<div class="wp-block-column is-vertically-aligned-bottom">
-				<!-- wp:paragraph {"align":"center","className":"has-text-align-center","fontSize":"smallest"} -->
-				<p class="has-text-align-center has-smallest-font-size"><a href="https://bootscout.org.uk/" data-type="URL" data-id="https://bootscout.org.uk/" target="_blank" rel="noreferrer noopener">Powered by Bootscout</a></p>
-				<!-- /wp:paragraph -->
-			</div>
-			<!-- /wp:column -->
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
+			<p style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)">Address:</p>
+			<!-- /wp:paragraph -->
 		</div>
-		<!-- /wp:columns -->
+		<!-- /wp:column -->
 	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:separator {"className":"is-style-wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"0"}},"color":{"background":"#ffffff5c"}}} -->
+	<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:0;background-color:#ffffff5c;color:#ffffff5c"/>
+	<!-- /wp:separator -->
+
+	<!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:group {"layout":{"type":"flex","allowOrientation":false,"justifyContent":"center"}} -->
+			<div class="wp-block-group">
+				<!-- wp:paragraph -->
+				<p>©</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:shortcode -->
+				[current-year]
+				<!-- /wp:shortcode -->
+
+				<!-- wp:site-title {"level":0} /-->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:paragraph {"className":"has-text-align-center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+			<p class="has-text-align-center" style="margin-top:0;margin-bottom:0">Charity number: </p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column"></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"bottom"} -->
+		<div class="wp-block-column is-vertically-aligned-bottom">
+			<!-- wp:paragraph {"align":"center","className":"has-text-align-center","fontSize":"smallest"} -->
+			<p class="has-text-align-center has-smallest-font-size"><a href="https://bootscout.org.uk/" data-type="URL" data-id="https://bootscout.org.uk/" target="_blank" rel="noreferrer noopener">Powered by Bootscout</a></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
 <!-- /wp:group -->
